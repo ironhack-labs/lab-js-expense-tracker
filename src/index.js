6463b46fead0a2dc6899b28c49aa3b73de9ac861
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-01-14 19:31:50 +0100

    iteration 4 also done but tests failing

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ class Entry {
 // Income
 class Income extends Entry {
   constructor(date, amount, description) {
-    super (date, amount, description);
+    super(date, amount, description);
     this.type = "income";
   }
 }
 
 // Expense
 class Expense extends Entry {
-  constructor (date, amount, description, paid) {
-    super (date, amount, description);
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
     this.paid = paid;
-    this.type = "expense"
+    this.type = "expense";
   }
   getFormattedAmount() {
     return `-${this.amount} €`;
@@ -31,4 +31,51 @@ class Expense extends Entry {
 }
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+    this.totalIncome = 0;
+    this.totalExpenses = 0;
+    this.currentBalance = 0;
+  }
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getTotalIncome() {
+    if (!this.entries.length) {
+      return 0;
+    }
+
+    this.totalIncome = 0;
+
+    this.entries.forEach((entry) => {
+      if (entry.type === "income") {
+        this.totalIncome += entry.amount;
+      }
+    });
+
+    return this.totalIncome;
+  }
+
+  getTotalExpense() {
+    if (!this.entries.length) {
+      return 0;
+    }
+
+    this.totalExpenses = 0;
+
+    this.entries.forEach((entry) => {
+      if (entry.type === "expense") {
+        this.totalExpenses += entry.amount;
+      }
+    });
+    return this.totalExpenses;
+  }
+
+  getCurrentBalance() {
+    if (!this.entries.length) {
+      return 0;
+    }
+    return totalIncome - totalExpenses;
+  }
+}
