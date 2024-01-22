fffc112fafafb3052786c6556ca98bae9ee27da9
Kumar Daryanani <destral@msn.com>
2024-01-22 22:01:42 +0100

    Lab complete

--- src/index.js
+++ src/index.js
@@ -42,21 +42,6 @@ class Budget {
     addEntry(newEntry) {
         this.entries.push(newEntry);    
     };
-    getTotal(type) {
-        let typeEntries = 0;
-        let typeTotal = 0;
-
-        this.entries.forEach(function(element) {
-            if (element.type === type) {
-                typeTotal += element.amount;
-                typeEntries += 1;
-            }
-        });
-        if (typeEntries === 0) {
-            return 0;
-        }
-        return typeTotal;
-    }
     getTotalIncome() {
         let incomeEntries = 0;
         let totalIncome = 0;
@@ -91,21 +76,64 @@ class Budget {
         if (this.entries.length === 0) {
             return 0;
         }
-        return this.getTotalIncome()-this.getTotalExpense();
+        return this.getTotal("income")-this.getTotal("expense");
     };
+    getTotal(type) {
+        let typeEntries = 0;
+        let typeTotal = 0;
+
+        this.entries.forEach(function(element) {
+            if (element.type === type) {
+                typeTotal += element.amount;
+                typeEntries += 1;
+            }
+        });
+        if (typeEntries === 0) {
+            return 0;
+        }
+        return typeTotal;
+    }
+    getFormattedEntries() {
+        const formattedArray = [];
+        this.entries.forEach(function(element) {
+            if (element.type === 'income') {
+                const formattedEntry = `${element.date} | ${element.description} | +${element.amount} €`;
+                formattedArray.push(formattedEntry);
+            } else if (element.type === 'expense') {
+                const formattedEntry = `${element.date} | ${element.description} | -${element.amount} €`;
+                formattedArray.push(formattedEntry)
+            }
+        });
+        return formattedArray;
+    }
 }
 
 //console.log(new Entry("03/10", 200, "Dinner with friends"))
 
 const myBudget = new Budget();
-const myIncome1 = new Income("10/1", 200, "sales");
-const myIncome2 = new Income("11/1", 350, "sales");
-const myExpense1 = new Expense("11/1", 150, "groceries", true);
+//const budget1 = new Budget();
+const budget2 = new Budget();
+const income1 = new Income("2024-06-17", 200, "food");
+const income2 = new Income("2024-06-17", 200, "food");
+const expense1 = new Expense("2024-06-17", 100, "food", true);
+const expense2 = new Expense("2024-06-17", 500, "food", false);
+
+myBudget.addEntry(income1);
+myBudget.addEntry(income2);
+myBudget.addEntry(expense1);
+myBudget.addEntry(expense2);
+/*
+const myIncome1 = new Income("2024-10-01", 200, "sales");
+const myIncome2 = new Income("2024-11-01", 350, "sales");
+const myExpense1 = new Expense("2024-11-01", 150, "groceries", true);
 myBudget.addEntry(myIncome1);
 myBudget.addEntry(myIncome2);
 myBudget.addEntry(myExpense1);
-
+*/
+/*
 console.log(myBudget.entries);
-//console.log(myBudget.getCurrentBalance());
 console.log(myBudget.getTotal("income"))
 console.log(myBudget.getTotal("expense"))
+console.log(myBudget.getCurrentBalance())
+*/
+console.log(myBudget.getFormattedEntries())
