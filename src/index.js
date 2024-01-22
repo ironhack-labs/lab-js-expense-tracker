7c4ddad7bc561b758b80d1107c6767ee5640c9f3
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-22 15:17:15 +0100

    not yet done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,84 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    console.log(`${this.amount} €`);
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super();
+
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super();
+
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.paid = paid;
+    this.type = "expense";
+  }
+
+  getFormattedAmount() {
+    console.log(`-${this.amount} €`);
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget extends Entry {
+  constructor() {
+    super();
+    this.entries = [];
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getTotalIncome() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    let sum = 0;
+    this.entries.forEach((element) => {
+      sum += element;
+      console.log(sum);
+      return sum;
+    });
+  }
+  getTotalExpense() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+        return 0;
+      }
+    return this.getTotalIncome() - this.getTotalExpense() 
+  }
+}
+
+// const budget = new Budget();
+// const income = new Entry("2024-06-17", 100, "food");
+// const expense = new Entry("2024-06-17", 100, "food", true);
+// budget.addEntry(36);
+// budget.addEntry(2);
+
+// console.log(budget.getTotalIncome());
