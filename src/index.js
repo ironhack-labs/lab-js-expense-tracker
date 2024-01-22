4a11caf0ada636d53e22f88f0c88138f65aa7a85
Laia Navalon <laianavalon@gmail.com>
2024-01-22 15:41:18 +0100

    in the middle of getTotalIncome

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
+
+const type = new Income();
 
 // Expense
-class Expense {}
+class Expense extends Income {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+
+    getTotalIncome() {
+        let sum = 0;
+        this.entries.forEach(function(element) {
+            if (this.type === "income") {
+                sum += element;
+            }
+        })
+        return sum;
+    }
+
+    getTotalExpense() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+    }
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+    }
+}
