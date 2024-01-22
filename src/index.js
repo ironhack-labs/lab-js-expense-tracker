205d263f9a20d7799ec8a3d80256fdb159bf4c6f
Rui Duram <ruiduran.jr@gmail.com>
2024-01-22 15:50:08 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return this.amount + " " + "€";
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+    this.totalIncome = 0;
+    this.totalExpenses = 0;
+    this.balance;
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getTotalIncome() {
+    this.entries.forEach((elem) => {
+      if (elem.type === "income") {
+        this.totalIncome += elem.amount;
+      }
+    });
+    return this.totalIncome;
+  }
+  getTotalExpense() {
+    this.entries.forEach((entry) => {
+      if (entry.type === "expense") {
+        this.totalExpenses += entry.amount;
+      }
+    });
+    return this.totalExpenses;
+  }
+  getCurrentBalance() {
+    this.expenses = this.getTotalExpense();
+    this.income = this.getTotalIncome();
+    this.balance = this.income - this.expenses;
+    return this.balance;
+  }
+}
