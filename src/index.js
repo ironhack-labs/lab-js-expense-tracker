c3930697849bfc47638f88971428072218197d1c
Vituin <124085647+Vituin@users.noreply.github.com>
2024-01-22 21:37:18 +0100

    No solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(dateValue, amountValue, descriptionValue) {
+        this.date = dateValue
+        this.amount = amountValue
+        this.description = descriptionValue
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
+
+
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description)
+        this.type = "income"
+    }
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paidValue) {
+        super(date, amount, description)
+        this.paid = paidValue
+        this.type = "expense"
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+
+    }
+}
+
+class AddEntry extends Budget {
+    constructor() {
+
+    }
+}
