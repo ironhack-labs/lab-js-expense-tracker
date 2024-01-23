0fe29c82d042c2dbdd83b62c6d9d8c7c12d534c4
Haripriya <haripriyagct@gmail.com>
2024-01-23 10:05:11 +0100

    edited

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class Entry {
         this.description = description;
     }
     getFormattedAmount() {
-        return (`${this.amount} €`)
+        return (`${this.amount} €`);
     }
 }
 
@@ -14,7 +14,7 @@ class Entry {
 class Income extends Entry {
 
     constructor(date, amount, description) {
-        super();
+        super(date, amount, description);
         this.type = "income";
         this.date = date;
         this.amount = amount;
@@ -26,7 +26,7 @@ class Income extends Entry {
 // Expense
 class Expense extends Entry {
     constructor(date, amount, description, paid) {
-        super();
+        super(date, amount, description);
         this.type = "expense";
         this.date = date;
         this.amount = amount;
