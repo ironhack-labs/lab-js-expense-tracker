54e179fee71a9005af6d8a745964df7e25eda0b6
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-01-30 00:02:39 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+
+    }
+
+        //metodo
+        getFormattedAmount() {
+            return `${this.amount} €`
+        }
+}
+
+
 
 // Income
-class Income {}
+class Income extends Entry {
+
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.income = income;
+    }
+
+    
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+
+    constructor (date, amount, description, paid) {
+        super(date, amount, description);
+        this.paid = paid;
+
+    }
+    getFormattedAmount() {
+        return `- ${this.amount} €`
+    }
+
+}
 
 // Budget
-class Budget {}
+class Budget {
+
+    constructor () {
+        this.entries = [];
+    }
+
+
+    addEntry(income) {
+        this.entries.push(income);
+    }
+    
+    getTotalIncome() {}
+    getTotalExpense() {
+
+    }
+    getCurrentBalance() {}
+
+
+
+
+
+
+}
