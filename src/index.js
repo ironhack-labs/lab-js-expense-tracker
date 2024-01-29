a39efbf0e97bcc1403d545f0e9b9ca4c950c3b79
Josep Climent <josepcliment92@gmail.com>
2024-01-29 20:26:52 +0100

    nueva version

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 // Entry
-class Entry {}
+class Entry {
+constructor(date, amount, description) {
+this.date = date;
+this.amount = amount;
+this.description = description;
+}
+getFormattedAmount() {
+    return `${this.amount} €`
+}
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+constructor(date, amount, description) {
+    super(date, amount, description)
+    this.type = "income"; 
+}
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+constructor(date, amount, description, category) {
+    super(date, amount, description);
+    this.paid = category; 
+    this.type = "expense";
+}
+getFormattedAmount() {
+    return `-${this.amount} €`
+}
+
+}
 
 // Budget
-class Budget {}
+class Budget {
+constructor(){
+    this.entries = [];
+}
+
+    addEntry(newEntry){
+        this.entries.push[newEntry]; 
+
+    }
+    getTotalIncome(){
+
+    }
+    getTotalExpense(){
+
+    }
+    getCurrentBalance(){
+
+    }
+
+
+}
