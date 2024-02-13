ff3d5b713c7f45953dce693896020e0eb4b5a2bb
JaserLaser <158750163+JaserLaser@users.noreply.github.com>
2024-02-13 13:11:37 -0400

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date
+        this.amount = amount
+        this.description = description
+    
+    }
+   getFormattedAmount(){
+    return `${this.amount} €`
+   } 
+}
 
-// Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description,)
+        this.type = "income"
+    }
+}
 
-// Expense
-class Expense {}
 
-// Budget
-class Budget {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, description,)
+        this.type = "expense"
+        this.paid = paid
+}
+        getFormattedAmount(){
+            return `-${super.getFormattedAmount()}`
+        }
+}
+
+
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+    addEntry(entry) {
+      this.entries.push(entry)  
+    }
+    getTotalIncome() {
+        let totalIncome = 0
+        this.entries.forEach((entry) => {
+            if (entry.type === "income"){
+                totalIncome += entry.amount
+            }
+        })
+        return totalIncome
+    }
+    getTotalExpense() {
+        let totalExpense = 0
+        this.entries.forEach((entry) => {
+            if (entry.type === "expense") {
+                totalExpense += entry.amount
+            }
+        })
+        return totalExpense
+    }
+    getCurrentBalance(){
+        return this.getTotalIncome() - this.getTotalExpense()
+    }
+}
