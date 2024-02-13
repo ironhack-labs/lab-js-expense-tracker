c4b32860e252b4a4a30bdcbecf68d2d2a2ac2cc8
Luis Emmanuel <johndoe@example.comgit config --global user.email lerodriguezreyes@outlook.com>
2024-02-12 23:43:45 -0400

    lab almost done, missing gettotal income and expense methods for budget class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date,
+        this.amount = amount,
+        this.description = description
+    }
+    
+    getFormattedAmount(){
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+   constructor(date, amount, description) {
+   super(date, amount, description);
+   this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor(date, amount, description, paid){
+        super(date,amount,description)
+        this.paid = paid
+        this.type = "expense"
+    }
+
+    getFormattedAmount(){
+        return `${-this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor () {
+    this.entries = []
+    }
+
+    addEntry(entry){
+        this.entries.push(entry)
+    }
+
+    getTotalIncome() {
+        let totalIncome = 0;
+        this.entries.forEach((entry) => {
+             {
+                totalIncome += entry.amount;
+            }
+        });
+        return totalIncome;
+    }
+
+    getTotalExpenses() {
+        let totalExpenses = 0;
+        this.entries.forEach((entry) => {
+            i {
+                totalExpenses += entry.amount;
+            }
+        });
+        return totalExpenses;
+    }
+
+    getCurrentBalance() {
+        return this.getTotalIncome() - this.getTotalExpenses();
+    }
+}
