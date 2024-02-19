3b16b9751d9cf4199952cb12723c6388ea6e6e6a
Memoryman2022 <rlacannon@gmail.com>
2024-02-19 15:33:29 +0100

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
-
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.type = "expense";
+    this.paid = paid;
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(newEntry) {
+    this.entries.push(newEntry);
+  }
+  getTotalIncome() {
+    let totIn = 0;
+    this.entries.forEach((e) => {
+      if (e instanceof Income) {
+        totIn += e.amount;
+      }
+    });
+    return totIn;
+  }
+  getTotalExpense() {
+    let totOut = 0;
+    this.entries.forEach((e) => {
+      if (e instanceof Expense) {
+        totOut += e.amount;
+      }
+    });
+    {
+      return totOut;
+    }
+  }
+  getCurrentBalance() {
+    let balance = 0;
+    this.entries.forEach((e) => {
+      balance.push(e(totIn.amount - totOut.amount));
+    });
+    return balance;
+  }
+}
