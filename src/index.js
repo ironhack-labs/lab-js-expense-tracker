1e76847e598da8e9950d45ee78708b26e0b1008c
Rafa <rafaokc33@gmail.com>
2024-03-14 20:02:39 +0100

    Iteration 1, 2 and 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount (){
+    let money = this.amount + " €";
+    return money; 
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income"; 
+  }
+  }
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense"; 
+  }
+  getFormattedAmount (){
+    let money = "-" + this.amount + " €";
+    return money; 
+  }
+  }
 
 // Budget
 class Budget {}
