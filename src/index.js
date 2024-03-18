68ca070db9f69b9355eb0c56984f28df6d6a0c36
James Myles <myles.james@gmail.com>
2024-03-18 15:15:29 +0100

    Finished lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description, paid);
+        this.paid = paid;
+        this.type = "expense";
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+    getCurrentBalance(){
+        let totalBudget = 0;
+        
+        for (let i = 0; i < this.entries.length; i++) {
+            totalBudget += parseInt(this.entries[i].getFormattedAmount());
+        }
+       return totalBudget;
+    }
+};
+
+
+
