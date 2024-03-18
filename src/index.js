6b93d33eb97ec39d9ae4471aceb8c828297a114c
tiagosarmentonogueira <tiagosarnog@gmail.com>
2024-03-18 18:21:59 +0000

    almost solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date
+        this.amount = amount
+        this.description = description
+    }
+    getFormattedAmount() {
+        return (`${this.amount} €`)
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super (date, amount, description)
+        this.type = "income"
+    }
+}
+
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description)
+        this.date = date
+        this.amount = amount
+        this.description = description
+        this.paid = paid
+        this.type = "expense"
+    }
+    getFormattedAmount() {
+        return (`-${this.amount} €`)
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+    addEntry(entry){
+        this.entries.push(entry)
+    }
+
+    getCurrentBalance() {  
+
+        if (balance = []) {
+            return 0
+        }
+    
+    }
+
+}
+
+
+
