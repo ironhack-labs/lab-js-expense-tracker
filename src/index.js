24ccb3cefade62e382b8960cb3c718c1583d395e
dieegoolimaa <dieegoolimaa@gmail.com>
2024-03-18 13:04:20 +0000

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    };
+
+    getFormattedAmount(){
+        return `${this.amount} €`
+    };
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description, "Income");
+        this.type = "income"; 
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description, "Expense");
+        this.paid = paid;
+        this.type = "expense"
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(newEntry) {
+        this.entries.push(newEntry);
+    }
+
+    getCurrentBalance() {
+        let total = 0;
+
+        for (let entry of this.entries) {
+            if (entry.type === "income") {
+                total += entry.amount;
+            } else if (entry.type === "expense") {
+                total -= entry.amount;
+            }
+        }
+
+        return total
+    }
+}
