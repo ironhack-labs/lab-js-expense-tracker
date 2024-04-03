8dd7344dbd807414d44f6f0330b54c0de31dde2b
Noelia <sharimanoe@gmail.com>
2024-04-03 18:40:40 +0200

    Lab solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,87 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date= date;
+        this.amount= amount;
+        this.description= description;
+    }
+
+    getFormattedAmount(){
+        return (`${this.amount} €`);
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.type= "income";
+    }    
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description,paid){
+        super(date, amount, description);
+        this.paid=paid;
+        this.type= "expense";
+    }
+
+    getFormattedAmount(){
+        return (`-${this.amount} €`);
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+
+    constructor(){
+        this.entries=[];
+    }
+
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+
+    getCurrentBalance(){
+       let auxSumExpense =0;
+
+        if (this.entries.length===0){
+            return 0;
+        }
+        
+        this.entries.forEach(function(entry){
+            if (entry.type === "expense"){
+                auxSumExpense -=entry.amount;
+                console.log(entry.amount)
+            } else if (entry.type=== "income"){
+                auxSumExpense +=entry.amount;
+            }})
+            console.log("dont")
+
+        // for(let i =0; i<this.entries.length;i++){
+        //     if(this.entries[i].type ==="expense"){
+        //         auxSumExpense -= this.entries[i].amount
+        //         console.log(auxSumExpense,"test")
+        //     }
+        //     else{
+        //         auxSumExpense += this.entries[i].amount
+        //     }
+        // }
+        // console.log(auxSumExpense)
+        return auxSumExpense;
+    }
+}
+
+
+// const income1 = new Income("2024-06-17", 200, "food");
+// console.log(income1)
+// const income2 = new Income("2024-06-17", 200, "food");
+// const expense1 = new Expense("2024-06-17", 100, "food", true);
+
+// let test = new Budget()
+// test.addEntry(income1)
+// test.addEntry(income2)
+// test.addEntry(expense1)
+
+// test.getCurrentBalance()
