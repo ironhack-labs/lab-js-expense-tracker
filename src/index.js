6234223de2fea73bbcb282432a5fcde25ba48119
Stephan Granzow <stephan_granzow@web.de>
2024-04-03 19:26:07 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,81 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+
+    this.type = "income";
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.paid = true;
+    this.type = "expense";
+  }
+
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    let totalExpense = 0;
+    let totalIncome = 0;
+    for (let i = 0; i < this.entries.length; i++) {
+      if (this.entries[i].type === "expense") {
+        totalExpense += this.entries[i].amount;
+      } else {
+        totalIncome += this.entries[i].amount;
+      }
+    }
+    return totalIncome - totalExpense;
+  }
+  getFormattedEntries() {
+    let formattedEntries = [];
+    this.entries.forEach((element) => {
+      if (element.type === "expense") {
+        formattedEntries.push(
+          `${element.date} | ${element.description} | -${element.amount} €`
+        );
+      } else {
+        formattedEntries.push(
+          `${element.date} | ${element.description} | ${element.amount} €`
+        );
+      }
+    });
+    return formattedEntries;
+  }
+}
