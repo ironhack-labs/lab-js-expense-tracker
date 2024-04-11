640fd55f2a3291aad7f7ee966f5d480d24755a26
Suaste Cherizola <jsuaste86@gmail.com>
2024-04-11 16:58:40 +0200

    first 5 done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date,amount,description) {
+        this.date = date,
+        this.amount = amount,
+        this.description = description
+    }
+    getFormattedAmount() {
+        return `amount`
+    }
+
+}
+
+/* should take 3 arguments (date, amount, description).
+getFormattedAmount() method
+should be defined
+should receive 0 arguments
+should return the amount string with the amount and the € symbol (format: "AMOUNT €") */
 
 // Income
 class Income {}
