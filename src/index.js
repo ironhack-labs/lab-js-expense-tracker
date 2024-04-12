b0f5c9e49c02c4fa7a96f7328a379994a360f3c4
Suaste Cherizola <jsuaste86@gmail.com>
2024-04-12 11:36:40 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -6,22 +6,56 @@ class Entry {
         this.description = description
     }
     getFormattedAmount() {
-        return `amount`
+        return this.amount + " €"
     }
-
 }
 
-/* should take 3 arguments (date, amount, description).
-getFormattedAmount() method
-should be defined
-should receive 0 arguments
-should return the amount string with the amount and the € symbol (format: "AMOUNT €") */
-
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super (date,amount, description);
+        this.type = "income"
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor (date, amount, description, category) {
+        super (date,amount, description);
+        this.paid = category;
+        this.type = "expense"
+    }
+    getFormattedAmount() {
+        return "-" + this.amount + " €"
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+    addEntry(newEntry) {
+        this.entries.push(newEntry)
+    }
+    getCurrentBalance(){
+        if (this.entries.length === 0) {
+            return 0
+        }
+        let balance = 0;
+        this.entries.forEach(element => 
+            element.type === "income"? balance += element.amount : balance -= element.amount     
+        )
+        return balance
+    }
+    getFormattedEntries() {
+        let result= [];
+        this.entries.forEach(entrie => {
+          entrie.type === "income"?
+          result.push(`${entrie.date} | ${entrie.description} | ${entrie.amount} €`)
+          :
+          result.push(`${entrie.date} | ${entrie.description} | -${entrie.amount} €`)
+        })
+        return result
+    }
+}
