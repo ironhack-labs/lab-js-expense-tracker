a55b2f97966f7345dcc2962f091ab6d917afa7f7
Diego Cerezo <diego.cerezo@gmail.com>
2024-04-15 19:42:16 +0200

    Sooolveeed!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,96 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(dateValue, amountValue, descriptionValue) {
+        this.date = dateValue
+        this.amount = amountValue
+        this.description = descriptionValue
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(dateValue, amountValue, descriptionValue) {
+
+        super(dateValue, amountValue, descriptionValue)
+
+        this.type = "income"
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(dateValue, amountValue, descriptionValue, paidValue) {
+
+        super(dateValue, amountValue, descriptionValue)
+        this.paid = paidValue
+        this.type = "expense"
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+
+    addEntry(entries) {
+        this.entries.push(entries)
+
+    };
+
+    getCurrentBalance() {
+
+        if (this.entries.length === 0) {
+            return 0
+        }
+
+        let totalIncome = 0
+        let totalExpense = 0
+        let balance = 0
+
+        this.entries.forEach((eachEntry) => {
+            if (eachEntry.type === "income") {
+                totalIncome += (eachEntry.amount)
+            }
+            else if (eachEntry.type === "expense") {
+                totalExpense += (eachEntry.amount)
+            }
+
+        })
+
+        balance = totalIncome - totalExpense
+        return balance
+
+    }
+
+    getFormattedEntries() {
+
+        let budgetEntries = []
+
+        this.entries.forEach((eachEntry) => {
+            if (eachEntry.type === "income") {
+                budgetEntries.push(`${eachEntry.date} | ${eachEntry.description} | ${eachEntry.amount} €`)
+            }
+            else if (eachEntry.type === "expense") {
+                budgetEntries.push(`${eachEntry.date} | ${eachEntry.description} | -${eachEntry.amount} €`)
+            }
+        })
+        return budgetEntries
+    }
+
+
+
+}
+
+
+
