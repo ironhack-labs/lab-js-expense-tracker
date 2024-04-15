f3ec95a9560ff2e397fd64ba24ba55977b379c9b
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-04-15 15:26:51 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    };
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    };
+};
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    };
+};
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    };
+
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    };
+};
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    };
+
+    addEntry(entryObject) {
+        this.entries.push(entryObject);
+    };
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        };
+        let sum = 0;
+        this.entries.forEach(element => {
+            if (element.type === "expense") {
+                sum -= element.amount;
+            } else {
+                sum += element.amount;
+            };
+        });
+        return sum
+    };
+
+    getFormattedEntries() {
+        const arrayFormatted = [];
+        this.entries.forEach(element => {
+            arrayFormatted.push(`${element.date} | ${element.description} | ${element.getFormattedAmount()}`)
+        });
+        return arrayFormatted;
+    };
+};
