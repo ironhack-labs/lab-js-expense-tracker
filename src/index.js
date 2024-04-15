552f7bc7e8070c523dabb2d512eec5457995a228
Natan Simoes <natan16simoes@gmail.com>
2024-04-15 14:56:45 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date,amount,description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date,amount,description,paid) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = paid;
+    
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+    
+}
 
 // Budget
-class Budget {}
+
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(newEntry) {
+        this.entries.push(newEntry);
+    }
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+
+        let totalIncome = 0;
+        let totalExpense = 0;
+
+        this.entries.forEach(entry => {
+            if (entry instanceof Income) {
+                totalIncome += entry.amount;
+            } else if (entry instanceof Expense) {
+                totalExpense += entry.amount;
+            }
+        });
+
+        return totalIncome - totalExpense;
+    }
+}
+
