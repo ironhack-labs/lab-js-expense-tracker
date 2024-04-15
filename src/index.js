2ba4d1ffc4df60ad643dd9316c4f251cec46eb3d
Yassine Moussaif <yassine.moussaif1234@gmail.com>
2024-04-15 15:44:22 +0200

    "lab1_week2"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
 // Entry
-class Entry {}
+class Entry {
 
+constructor(date,amount,description ){
+
+    this.date=date;
+    this.amount=amount;
+    this.description=description;
+}
+getFormattedAmount(){
+    return(`${this.amount} €`);
+}
+}
+ 
 // Income
-class Income {}
+class Income extends Entry {
+constructor(date,amount,description){
+super(date,amount,description);
+this.type="income";
+}
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+
+    constructor(date,amount,description,paid){
+        super(date,amount,description)
+        this.type="expense";
+        this.paid=paid;
+    }   
+
+getFormattedAmount(){
+    return(`-${this.amount} €`);
+}
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries=[];
+    }
+
+    addEntry(myEntry){
+
+      
+        this.entries.push(myEntry);
+
+    }
+
+    getCurrentBalance(){
+        let totalIncome = this.entries
+      .filter(entry => entry.type === "income")
+      .reduce((acc, curr) => acc + curr.amount, 0);
+    let totalExpenses = this.entries
+      .filter(entry => entry.type === "expense")
+      .reduce((acc, curr) => acc + curr.amount, 0);
+    return totalIncome - totalExpenses;
+
+    }
+
+}
