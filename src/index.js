21ffbedf4e50ed0c4f9efd222b8b2784bd76f747
Manuele Vancin <mfvancin@gmail.com>
2024-04-15 15:04:07 +0100

    LabsW2D1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 // Entry
-class Entry {}
+
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    };
+
+    getFormattedAmount() {
+        return `${amount} €`;
+    };
+};
 
 // Income
-class Income {}
+
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super (date, amount, description)
+    };
+
+    getFormattedAmount() {
+        return `${amount} €`;
+    };
+};
+
+const earn = new Income ();
 
 // Expense
-class Expense {}
+
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super (date, amount, description)
+        this.paid = paid;
+    };
+
+    getFormattedAmount() {
+        return `${amount} €`;
+    };
+};
 
 // Budget
-class Budget {}
+
+class Budget {
+    addEntry() {
+
+    };
+
+    getCurrentBalance() {
+        if (Entry === 0) {
+            return 0;
+        }
+        return Entry - Expense;
+    };
+}
+
