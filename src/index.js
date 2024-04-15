6ab4104b53efae2f62e4ccc7590ad3325bd3d30e
Paloma Rehecho <paloma_re@hotmail.com>
2024-04-15 18:02:02 +0100

    auch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,70 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(dateValue, amountValue, descriptionValue) {
+        this.date = dateValue
+        this.amount = amountValue
+        this.description = descriptionValue
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(dateValue, amountValue, descriptionValue) {
+        super(dateValue, amountValue, descriptionValue)
+        this.type = "income"
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(dateValue, amountValue, descriptionValue, paid) {
+        super(dateValue, amountValue, descriptionValue, paid)
+        this.type = "expense"
+        this.paid = paid
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+}
+
+
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+
+    addEntry(entry) {
+        this.entries.push(entry)
+    }
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0
+        }
+
+        let finalBalance = 0
+        this.entries.forEach((eachEntry) => {
+            if (eachEntry.type === "income") {
+                finalBalance += eachEntry.amount
+            } else if (eachEntry.type === "expense") {
+                finalBalance -= eachEntry.amount
+            }
+        })
+        return finalBalance
+    }
+
+
+
+    getFormattedEntries() {
+
+    }
+}
+
+
+
