c066ab2d986f60757e547176f8717decd051f2c3
Claire Lacanal <lacanal.claire@gmail.com>
2024-04-15 15:57:48 +0200

    lab presque fini

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date,
+        this.amount = amount,
+        this.description = description
+    }
+    getFormattedAmount(){
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.date = date,
+        this.amount = amount,
+        this.description,
+        this.type = "income"
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, true);
+        this.date = date,
+        this.amount = amount,
+        this.description = description,
+        this.paid = paid,
+        this.type = "expense"
+    }
+    getFormattedAmount(){
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = []
+    }
+    addEntry(Expense){
+        this.entry = this.entries.push(Expense);
+    }
+    getCurrentBalance(){
+        if(!this.entry){
+            return 0;
+        }else{
+            return this.entries.Income - this.entries.Expense;
+        }
+
+    }
+}
