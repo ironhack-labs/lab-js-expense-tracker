1c4aa10a775fb44f3406f35a0072555a5f5cb9c4
unknown <alexanderdinsmore@yahoo.com>
2024-05-06 15:22:51 -0400

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,79 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date
+        this.amount = amount
+        this.description = description
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
+    
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description)
+        this.type = 'income'
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, description)
+        this.type = 'expense'
+        this.paid = paid
+    }
+
+    getFormattedAmount() {
+        // return `-${this.amount} €`
+        return `-${super.getFormattedAmount()}`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = []
+    }
+
+    addEntry(entry) {
+        this.entries.push(entry)
+    }
+
+    getCurrentBalance() {
+
+        let balance = 0
+
+        this.entries.forEach((entry) => {
+            if (entry.type === 'income') {
+                balance += entry.amount
+            } else {
+                balance -= entry.amount
+            }
+        })
+
+        return balance
+
+    }
+    
+    getFormattedEntries() {
+        
+        let theseEntries = []
+
+        this.entries.forEach((entry) => {
+
+            let returnString = `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`
+            theseEntries.push(returnString)
+        
+        })
+
+        return theseEntries
+
+    }
+}
+
+let budget1 = new budget1()
