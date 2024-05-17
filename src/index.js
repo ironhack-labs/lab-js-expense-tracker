2acc00035a22e029a27a592e99f52bc8dc171ae6
Ana Rosa <guilherme.fontes98@outlook.com>
2024-05-17 09:33:51 +0100

    labdone

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
-// Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
 
-// Income
-class Income {}
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
-// Expense
-class Expense {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
-// Budget
-class Budget {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
+
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+
+  getCurrentBalance() {
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    this.entries.forEach((entry) => {
+      if (entry.type === "income") {
+        totalIncome += entry.amount;
+      } else if (entry.type === "expense") {
+        totalExpense += entry.amount;
+      }
+    });
+
+    return totalIncome - totalExpense;
+  }
+}
