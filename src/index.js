2e324c966fca90e3e92f7aaa67d93f10234cf988
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-10 15:03:25 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,64 @@
 // Entry
-class Entry {}
-
-// Income
-class Income {}
-
-// Expense
-class Expense {}
-
-// Budget
-class Budget {}
+class Entry {
+    constructor(date, amount, description) {
+      this.date = date;
+      this.amount = amount;
+      this.description = description;
+    }
+  
+    getFormattedAmount() {
+      return `${this.amount} €`;
+    }
+  }
+  
+  // Income
+  class Income extends Entry {
+    constructor(date, amount, description, type = "income") {
+      super(date, amount, description);
+      this.type = type;
+    }
+  }
+  
+  // Expense
+  class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+      super(date, amount, description);
+      this.paid = paid;
+      this.type = "expense";
+      this.category = this.paid;
+    }
+  
+    getFormattedAmount() {
+      return `-${this.amount} €`;
+    }
+  }
+  
+  // Budget
+  class Budget {
+    constructor() {
+        this.entries = [];
+      }
+    
+      addEntry(newEntry) {
+        this.entries.push(newEntry);
+      }
+    
+      getCurrentBalance() {
+        if (this.entries.length === 0) return 0;
+    
+        let totalIncome = 0;
+        let totalExpense = 0;
+    
+        this.entries.forEach(entry => {
+          if (entry instanceof Income) {
+            totalIncome += entry.amount;
+          } else if (entry instanceof Expense) {
+            totalExpense += entry.amount;
+          }
+        });
+    
+        return totalIncome - totalExpense;
+      } 
+  }
+  
+  
