6a018d1fd95cb321a4f71513ac501ddebcc55e0d
Jose Copeti <jrcopeti@gmail.com>
2024-06-24 15:28:06 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,74 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+  }
+
+  type = "income";
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+  }
+  type = "expense";
+
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    return this.entries.reduce(
+      (acc, entry) => {
+        return entry.type === "income"
+          ? acc + entry.amount
+          : acc - entry.amount;
+      },
+
+      0
+    );
+  }
+
+  getFormattedEntries() {
+    const formattedEntries = [];
+
+    this.entries.forEach((entry) => {
+      const entrys = `${entry.date} | ${entry.description} | ${
+        entry.type === "income" ? "" : "-"
+      }${entry.amount} €`;
+
+      formattedEntries.push(entrys);
+    });
+
+    return formattedEntries;
+  }
+}
