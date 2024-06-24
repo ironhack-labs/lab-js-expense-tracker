96c93b600e5e4e67d45174e9bbd1b1a318689498
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-06-24 18:05:40 +0200

    FT_RMT_2406_IVAN

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  addEntry(par1) {
+    this.entries.push(par1);
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    let totalIncome = 0;
+
+    this.entries.forEach((eachEntry) => {
+      if (eachEntry.type === "income") {
+        totalIncome += eachEntry.amount;
+      } else if (eachEntry.type === "expense") {
+        totalIncome -= eachEntry.amount;
+      }
+    });
+
+    return totalIncome;
+  }
+  constructor() {
+    this.entries = [];
+  }
+}
