de4cc4ce97dde575af928df261d7043949357117
Sven Ole Spindler <s.spindler@gmx.de>
2024-06-25 09:16:29 +0200

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,68 +1,69 @@
 // Entry
 class Entry {
-    constructor(date,amount,description) {
-        this.date = date;
-        this.amount = amount;
-        this.description = description;
-    };
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
 
-    getFormattedAmount() {
-        const formattedAmount = `${this.amount} €`;
-        return formattedAmount; 
-    }
+  getFormattedAmount() {
+    const formattedAmount = `${this.amount} €`;
+    return formattedAmount;
+  }
 }
 
 // Income
 class Income extends Entry {
-        constructor (date,amount,description) {
-          // `super` refers to the constructor of the parent (Animal)
-          // with super Income gets all the attributes and methods of the Entry class
-          super(date, amount, description);
-          this.type = "income"; // <== a new attribute, just for Income
-        }
-      }
-
-
-// Expense
-class Expense extends Income {
-    constructor (date,amount,description,paid) {
-      // `super` refers to the constructor of the parent (Animal)
-      // with super Income gets all the attributes and methods of the Entry class
-      super(date, amount, description);
-      this.paid = paid
-      this.type = "expense"; // <== a new attribute, just for Income
-    }
-    getFormattedAmount() {
-        const formattedAmount = `-${this.amount} €`;
-        return formattedAmount; 
-    }  
+  constructor(date, amount, description) {
+    // `super` refers to the constructor of the parent (Animal)
+    // with super Income gets all the attributes and methods of the Entry class
+    super(date, amount, description);
+    this.type = "income"; // <== a new attribute, just for Income
+  }
 }
 
-
+// Expense
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    // `super` refers to the constructor of the parent (Animal)
+    // with super Income gets all the attributes and methods of the Entry class
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense"; // <== a new attribute, just for Income
+  }
+  getFormattedAmount() {
+    const formattedAmount = `-${this.amount} €`;
+    return formattedAmount;
+  }
+}
 
 // Budget
 class Budget {
+  constructor() {
+    this.entries = [];
+  }
 
-    constructor() {
-        this.entries = []
-    }
-
-    addEntry(newEntry) {
+  addEntry(newEntry) {
     this.entries.push(newEntry);
-    }
-    
-    getCurrentBalance() {
-        this.totalBalance = 0;
-        
-        if (!this.entries.length) {return 0};
-    
+  }
 
-        for (let i = 0; i<this.entries.length; i++) {
-            if (this.entries[i].type === "expense") {
-                this.totalBalance -= this.entries[i].amount;
-            } else {this.totalBalance += this.entries[i].amount}
-        }
-/*        for (amount in Expense) {
+  getCurrentBalance() {
+    this.totalBalance = 0;
+    this.totalExpense = 0;
+    this.totalIncome = 0;
+
+    if (!this.entries.length) {
+      return 0;
+    }
+
+    for (let i = 0; i < this.entries.length; i++) {
+      if (this.entries[i].type === "expense") {
+        this.totalExpense += this.entries[i].amount;
+      } else {
+        this.totalIncome += this.entries[i].amount;
+      }
+    }
+    /*        for (amount in Expense) {
             totalExpense += this.amount
         }
 
@@ -70,37 +71,19 @@ class Budget {
             totalIncome += this.amount
         }
 */
-        return this.totalBalance;
-    }
+    this.totalBalance = this.totalIncome - this.totalExpense;
+    return this.totalBalance;
+  }
 
-getFormattedEntries() {
-    const formattedEntries = ""
-}
+  getFormattedEntries() {
+    const formattedEntries = "";
+  }
 
-
-getFormattedAmount() {
+  getFormattedAmount() {
     const formattedArr = [];
-    
-    for (let i = 0; i<this.entries.length; i++) {
-        if (this.entries[i].type === "income") {
-            formattedArr.push(
-              `${number.date} | ${number.description} | ${number.amount} €`
-            );
-          } else {
-            formattedArr.push(
-              `${number.date} | ${number.description} | -${number.amount} €`
-            );
-          }
-        }
 
-    return formattedArr; 
-}  
-
-// Bonus
-getFormattedEntries() {
-    const formattedArr = [];
-    this.entries.forEach((number) => {
-      if (number.type === "income") {
+    for (let i = 0; i < this.entries.length; i++) {
+      if (this.entries[i].type === "income") {
         formattedArr.push(
           `${number.date} | ${number.description} | ${number.amount} €`
         );
@@ -109,14 +92,27 @@ getFormattedEntries() {
           `${number.date} | ${number.description} | -${number.amount} €`
         );
       }
+    }
+
+    return formattedArr;
+  }
+
+  // Bonus
+  getFormattedEntries() {
+    const formattedArr = [];
+    this.entries.forEach((entry) => {
+      if (entry.type === "income") {
+        formattedArr.push(
+          `${entry.date} | ${entry.description} | ${entry.amount} €`
+        );
+      } else {
+        formattedArr.push(
+          `${entry.date} | ${entry.description} | -${entry.amount} €`
+        );
+      }
     });
     return formattedArr;
   }
 }
 
-
-
 // BONUS
-
-
-
