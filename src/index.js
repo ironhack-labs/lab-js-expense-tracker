87779f44ed61caad6bfce667cf2bfe50f8999260
Mirko Fede <mirko@mirkoeffe.shop>
2024-06-25 09:20:19 +0200

    ..

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ class Income extends Entry {
 }
 
 // Expense
-// Expense
 class Expense extends Entry {
     constructor(date, amount, description, paid) {
         super(date, amount, description);
