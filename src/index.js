29814a665b32b8257dcd2398ecabe73fbc0dfde0
benjaminelliott <benjamin.elliott.be@gmail.com>
2024-06-25 12:34:52 +0200

    revisions

--- src/index.js
+++ src/index.js
@@ -60,26 +60,3 @@ class Budget {
     });
   }
 }
-
-const budget1 = new Budget();
-const budget2 = new Budget();
-const income1 = new Income("2024-06-17", 10, "other");
-const income2 = new Income("2024-06-17", 3456, "salary");
-const expense1 = new Expense("2024-06-17", 100, "food", true);
-const expense2 = new Expense("2024-06-17", 99, "food", true);
-budget1.addEntry(income1);
-budget1.addEntry(income2);
-budget1.addEntry(expense1);
-budget1.addEntry(expense2);
-
-budget2.addEntry(income1);
-budget2.addEntry(income2);
-budget2.addEntry(expense1);
-
-console.log(budget1.entries);
-console.log(budget2.entries);
-
-console.log(budget1.getCurrentBalance());
-console.log(budget2.getCurrentBalance());
-console.log(budget1.getFormattedEntries());
-console.log(budget2.getFormattedEntries());
