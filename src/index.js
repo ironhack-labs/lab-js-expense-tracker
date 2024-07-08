a47c9ecee6775ff9e00a41fccc513185ad6c11dc
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-07-08 16:15:54 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,82 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date,
+      this.amount = amount,
+      this.description = description
+  }
+
+  getFormattedAmount() {
+
+    return `${this.amount} €`
+
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description)
+    this.type = "income"
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description)
+    this.paid = paid
+    this.type = "expense"
+  }
+
+  getFormattedAmount() {
+
+    return `-${this.amount} €`
+
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = []
+  }
+
+  addEntry(newEntry) {
+
+    this.entries.push(newEntry)
+
+  }
+
+  getCurrentBalance() {
+
+    let balance = 0
+
+    if (this.entries.length === 0) {
+      return 0
+    }
+
+    this.entries.forEach(function (entry) {
+
+      if (entry.type === "income") {
+        balance += entry.amount
+      } else if (entry.type === "expense") {
+        balance -= entry.amount
+      }
+
+    })
+
+    return balance
+  }
+  getFormattedEntries() {
+
+    let arrEntriesFormatted = []
+
+    this.entries.forEach(function (entry) {
+      arrEntriesFormatted.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`)
+    })
+
+    return arrEntriesFormatted
+
+  }
+}
