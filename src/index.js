3b0ea6a7e44ddf8914844245e7f17e3b9a033783
Gonçalo Duarte <goncalopduarte23@gmail.com>
2024-07-22 14:19:13 +0100

    solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 // Entry
-class Entry {}
-
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description, paid);
+    this.type = "expense";
+    this.paid = paid;
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+    this.balance = 0;
+  }
+  addEntry(entryObj) {
+    this.entries.push(entryObj);
+    if (entryObj instanceof Income) {
+      this.balance += entryObj.amount;
+    } else if (entryObj instanceof Expense) {
+      this.balance -= entryObj.amount;
+    }
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    return this.balance;
+  }
+}
