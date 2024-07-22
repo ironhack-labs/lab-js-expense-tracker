b1f71ecabcacfd12f0c8c9b3904d8a8f3bd0d271
Daniela Pinto <daniela.pinto.123@hotmail.com>
2024-07-22 17:30:57 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,74 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount () {
+        return(`${this.amount} €`)
+    }
+}
 
 // Income
-class Income {}
+
+class Income extends Entry {
+    constructor (date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+        }
+}
 
 // Expense
-class Expense {}
+
+class Expense extends Entry {
+    constructor (date, amount, description, paid) {
+        super (date, amount, description);
+
+        this.paid = paid;
+        this.type = "expense";   
+    }
+    getFormattedAmount () {
+        return(`-${this.amount} €`)
+    }
+}
 
 // Budget
-class Budget {}
+
+class Budget {
+    constructor () {
+        this.entries = [];
+    }
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+    getCurrentBalance() {
+        if (this.entries.length === 0) return 0;
+
+        let total = 0;
+
+        this.entries.forEach(function(element) {
+            if (element.type === "income") {
+                total += element.amount;
+            }
+            if (element.type === "expense") {
+                    total -= element.amount;
+               }
+            });
+            return total;
+    } 
+    getFormattedEntries() {
+        let formattedEntriesArr = [];
+
+        this.entries.forEach (function (element) {
+            if (element.type === 'income') {
+                formattedEntriesArr.push (`${element.date} | ${element.description} | ${element.amount} €`);
+               
+            }
+            if (element.type === 'expense') {
+                formattedEntriesArr.push (`${element.date} | ${element.description} | -${element.amount} €`);
+            }
+            });
+            return formattedEntriesArr;
+    } 
+}
