9ecf0c35818c56fe9ac6055e845f9250fc76f7f8
Casperavr <casper@hhc.earth>
2024-07-22 14:45:34 +0200

    done and dusted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount(){
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry{
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.type = "income";
+    }
+
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor(date, amount, description, paid){
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+    getFormattedAmount(){
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(entryObj){
+        this.entries.push(entryObj);
+    }
+    getCurrentBalance(){
+        let sum = 0;
+        for(const entry of this.entries){
+            entry.type === "income" ? sum += entry.amount : sum -= entry.amount;
+        }
+        return sum;
+    }
+    getFormattedEntries(){
+        let formattedEntries = [];
+        this.entries.forEach(function(entry){
+            formattedEntries.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`)
+        })
+        return formattedEntries;
+    }
+}
