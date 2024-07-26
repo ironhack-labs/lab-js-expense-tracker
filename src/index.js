d2b5cbb09673a2b02dc1ae651e89e3932b7a81b9
Amrata MOHAMED <amrata.m.mohamed@gmail.com>
2024-07-26 08:19:17 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;        
+    }
+
+getFormattedAmount() {
+    return `$("this.amount") €`;
+}
+}
+
+let myEntry = new Entry("25-07-2024", 100, "New Item");
+
+console.log(myEntry.getFormattedAmount());
+
 
 // Income
-class Income {}
+
+class Income extends Entry {
+    constructor(date, amount, description) {
+
+        super(date);
+        this.amount = amount;
+        this.description = description;
+        this.type = "income";
+        }
+
+    }
+        
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+
+        constructor(date, amount, description, paid) {
+
+            super(date);
+            this.amount = amount;
+            this.description = description;
+            this.paid = paid;
+            this.type = "expense";    
+        }
+    
+    getFormattedAmount() {
+        return `-$("this.amount") €`;
+    }
+    }
+
+const myExpense = new Expense("25-07-2024", 100, "New item", true);
+
 
 // Budget
-class Budget {}
+class Budget {
+
+}
+
+    
