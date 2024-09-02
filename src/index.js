e386dcd393a155be9653e0eeec9009be3d5dfda7
Cristina Aguilera <developer.c.93.c@gmail.com>
2024-09-02 14:32:33 +0200

    primera copia

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 // Entry
-class Entry {}
+class Entry {
+
+    constructor(date, amount, description){
+
+        this.date = date
+        this.amount = amount
+        this.description = description
+
+    }
+    getFormattedAmount (){
+
+        return this.amount + " €"
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry{
+
+    constructor (date, amount, description){
+
+        super(date, amount, description)
+
+        this.type = "income"
+
+
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+
+    constructor (date, amount, description, paid){
+
+        super(date, amount, description)
+
+        this.type = "expense"
+        this.paid = paid
+    }
+
+    getFormattedAmount(){
+
+        return "- " + this.amount
+    }
+
+}
 
 // Budget
 class Budget {}
