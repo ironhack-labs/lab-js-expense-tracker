54d441e98fb419697584d88d463a88830e413156
Angela Ruiz <angela.ruiz.rodriguez.8@gmail.com>
2024-09-02 15:32:16 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+       super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, category) {
+       super(date, amount, description);
+       this.type = "expense";
+       this.paid = category;
+    }
+    getFormattedAmount () {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor () {
+        this.entries = [];
+    }
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+        let balance = 0;
+        for(let i = 0; i < this.entries.length; i++) {
+            if (this.entries[i].type === "income") {
+                balance += this.entries[i].amount;
+            }
+            else if (this.entries[i].type === "expense") {
+                balance -= this.entries[i].amount;
+            }
+        }
+        return balance;    
+    }
+
+    getFormattedEntries() {
+        let arrayFormatted = [];
+        for(let i = 0; i < this.entries.length; i++) {
+            if (this.entries[i].type === "income") {
+                arrayFormatted.push(`${this.entries[i].date} | ${this.entries[i].description} | ${this.entries[i].amount} €`);
+            }
+            else if (this.entries[i].type === "expense") {
+                arrayFormatted.push(`${this.entries[i].date} | ${this.entries[i].description} | -${this.entries[i].amount} €`);
+            }
+        }
+        return arrayFormatted;
+    }
+}
+
