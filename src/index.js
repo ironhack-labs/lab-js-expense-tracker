a780c721d4ae7f3ed1945199ea9adadb79e51337
Franziska Scheer <franzi-scheer@web.de>
2024-09-19 21:40:31 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,70 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) return 0;
+    let sum = 0;
+    for (let i = 0; i < this.entries.length; i++) {
+      let currentEntry = this.entries[i];
+      if (currentEntry.type === "income") sum += currentEntry.amount;
+      else sum -= currentEntry.amount;
+    }
+    return sum;
+  }
+    getFormattedEntries() {
+        const arr = []
+        let string;
+        this.entries.forEach(e => {
+            string = `${e.date} | ${e.description} | ${e.getFormattedAmount()}`
+            arr.push(string)
+        });
+        console.log(arr)
+        return arr;
+    }
+}
+
+const budget = new Budget();
+const income1 = new Income("240919", 49.99, "Dinner");
+
+budget.addEntry(income1);
+
+console.log(budget);
+console.log(income1);
