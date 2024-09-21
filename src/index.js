9ef13053117f772ec2774492fde782db10a9e077
Mirela Dragulescu <dragulescumirela@gmail.com>
2024-09-21 10:06:52 +0200

    Mirela's solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
+
+const expense = new Entry("10.07.2024", 100, "whatever");
+console.log(expense.getFormattedAmount());
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
+const income1 = new Income("10.07.2024", 100, "whatever");
+console.log(income1);
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description, paid);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
+const expense1 = new Expense("10.07.2024", 99, "whatever", true);
+console.log(expense1.getFormattedAmount());
 
 // Budget
-class Budget {}
+class Budget extends Entry {
+  constructor() {
+    this.entries = entries;
+    let entries = [];
+  }
+  addEntry(income) {}
+  getCurrentBalance(){
+    
+  }
+}
