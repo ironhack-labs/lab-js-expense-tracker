464c2b0cbbf0d77cde9469c9b22e59360c07c070
Sebastian Luft <sebastian.luft.gm@gmail.com>
2024-09-23 16:09:26 +0200

    Done incl. Bonus

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, category) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = category;
+    }
+
+    getFormattedAmount () {
+        return `${-this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries =[];
+    }
+
+    addEntry (entry) {
+        this.entries.push(entry);    
+    }
+
+    getCurrentBalance() {
+        let balance = 0;
+        this.entries.forEach((element) => (element.type === "expense") ? balance -= element.amount : balance +=element.amount);
+        return balance;
+    }
+
+    getFormattedEntries() {
+        let formattedStringArray = [];
+        this.entries.forEach((element) => formattedStringArray.push(`${element.date} | ${element.description} | ${(element.type === "expense")? -element.amount:element.amount} €`));
+        return formattedStringArray;
+    }
+}
+
+
