08f3aa54e6bf3fd45820138bb229d90f4438c338
YOUR NAME <94797132+PARAMET7@users.noreply.github.com>
2024-09-23 16:13:49 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description){
+    super(date, amount, description);
+    this.type = 'income';
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+  constructor(date, amount, description, paid){
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = 'expense';
+  }
+  getFormattedAmount(){
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+
+  addEntry(entry) {
+    this.entries.push(entry)
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0)
+      return 0;
+      const totalIncom = this.entries.filter(e => e.type ==='income').reduce((acc, incomes)=> acc +incomes.amount, 0);
+      const totalexpense = this.entries.filter(e => e.type ==='expense').reduce((acc, expense) => acc + expense.amount, 0);
+    return totalIncom-totalexpense;
+  }
+  getFormattedEntries(){
+    const formattedEntries = [];
+    
+    this.entries.forEach(entry => {
+      const formattedAmount = entry.getFormattedAmount();
+      formattedEntries.push(`${entry.date} | ${entry.description} | ${formattedAmount}`);
+    });
+
+  return formattedEntries;
+  }
+}
+
+console.log(Entry.amount());
