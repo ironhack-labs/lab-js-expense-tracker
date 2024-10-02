9b2fe51c8798949ebf38aafa08a4aed06bf465cd
Sven Andersson <sven@andersson.es>
2024-10-02 21:09:03 +0200

    Iteration 5 simplified

--- src/index.js
+++ src/index.js
@@ -1,80 +1,76 @@
 // Entry
 class Entry {
-  constructor(date, amount, description) {
-    this.date = date;
-    this.amount = amount;
-    this.description = description;
-  }
-  getFormattedAmount() {
-    return `${this.amount} €`;
-  }
+	constructor(date, amount, description) {
+		this.date = date;
+		this.amount = amount;
+		this.description = description;
+	}
+	getFormattedAmount() {
+		return `${this.amount} €`;
+	}
 }
 
 // Income
 class Income extends Entry {
-  constructor(date, amount, description) {
-    super(date, amount, description);
-    this.type = "income";
-  }
+	constructor(date, amount, description) {
+		super(date, amount, description);
+		this.type = "income";
+	}
 }
 
 // Expense
 class Expense extends Entry {
-  constructor(date, amount, description, paid) {
-    super(date, amount, description);
-    this.paid = paid;
-    this.type = "expense";
-  }
-  getFormattedAmount() {
-    return `-${this.amount} €`;
-  }
+	constructor(date, amount, description, paid) {
+		super(date, amount, description);
+		this.paid = paid;
+		this.type = "expense";
+	}
+	getFormattedAmount() {
+		return `-${this.amount} €`;
+	}
 }
 
 // Budget
 class Budget {
-  constructor() {
-    this.entries = [];
-  }
-  addEntry(transaction) {
-    this.entries.push(transaction);
-  }
-  getCurrentBalance() {
-    let result = 0;
-    for (let i = 0; i < this.entries.length; i++) {
-      if (this.entries[i].type === "income") {
-        result += this.entries[i].amount;
-      } else if (this.entries[i].type === "expense") {
-        result -= this.entries[i].amount;
-      }
-    }
-    return result;
-  }
-  getFormattedEntries() {
-    const list = [];
-    let entry = ""
-    
-    this.entries.forEach((element) => {
-        if(element.type === "income"){
-             entry = new Entry(element.date, element.amount, element.description)
-             
-             
-        } else if(element.type === "expense"){
-            entry = new Expense(element.date, element.amount, element.description)
-            console.log("dentro del else if", element.getFormattedAmount());
-        }
-      list.push(
-        `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`
-      );
-    });
+	constructor() {
+		this.entries = [];
+	}
+	addEntry(transaction) {
+		this.entries.push(transaction);
+	}
+	getCurrentBalance() {
+		let result = 0;
+		for (let i = 0; i < this.entries.length; i++) {
+			if (this.entries[i].type === "income") {
+				result += this.entries[i].amount;
+			} else if (this.entries[i].type === "expense") {
+				result -= this.entries[i].amount;
+			}
+		}
+		return result;
+	}
+	getFormattedEntries() {
+		const list = [];
+		let entry = "";
 
-    return list;
-  }
+		this.entries.forEach((element) => {
+			/* if (element.type === "income") {
+				entry = new Entry(element.date, element.amount, element.description);
+			} else if (element.type === "expense") {
+				entry = new Expense(element.date, element.amount, element.description);
+				//console.log("dentro del else if", element.getFormattedAmount());
+			} */ // This code is not necessary because we have already created the Entry and Expense variables, they already enter in the Array as Entry or Expense, no need reuse the Class again.
+			list.push(
+				`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`
+			);
+		});
+
+		return list;
+	}
 }
 
 const budget = new Budget();
 
-const budget1 = new Budget();
-const budget2 = new Budget();
 const income1 = new Income("2024-06-17", 200, "food");
 const income2 = new Income("2024-06-17", 200, "food");
 const expense1 = new Expense("2024-06-17", 100, "food", true);
@@ -96,4 +92,3 @@ console.log(budget2.getCurrentBalance());
 const formattedEntries = budget.getFormattedEntries();
 
 console.log(formattedEntries);
-
