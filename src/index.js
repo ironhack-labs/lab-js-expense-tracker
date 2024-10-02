c8982981f7d779c83416317d12c334c16138e16a
Tomás Martín <tmartindsgn@gmail.com>
2024-10-02 19:53:43 +0200

    Intercation_2_done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date, amount, description){
+
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount(){
+        return  `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+
+        super(date, amount, description);
+        this.type = "income";
+    }
+   
+
+
+}
 
 // Expense
 class Expense {}
