95d06b4a867d834522102a75ee665d448d8281bc
candela <candelap.r.p@gmail.com>
2024-10-06 16:15:28 +0200

    [WD-PT-RMT-SEP24]- candela rodriguez

--- src/index.js
+++ src/index.js
@@ -1,11 +1,75 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super (date, amount, description);
+        this.type = "income";
+    }       
+    
+ }
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description)
+        this.paid = paid;
+        this.type = "expense";
+    }
+    
+    getFormattedAmount() {
+        return `-${this.amount} €`
+    }
+
+}
 
 // Budget
-class Budget {}
+class Budget { 
+    constructor() {
+        this.entries = []
+
+    }
+    addEntry(income) {
+        this.entries.push(income)
+
+    }
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+
+        let balance = 0;
+        this.entries.forEach(entry => {
+            if (entry.type === "income") {
+                balance += entry.amount;
+            } 
+            else if (entry.type === "expense") {
+                balance -= entry.amount;
+            }
+
+        })
+        return balance;
+    } 
+    
+    getFormattedEntries() {
+        let formattedEntries = []
+        this.entries.forEach(entry => {
+            const formattedEntry = `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`;
+            formattedEntries.push(formattedEntry);
+        })
+        return formattedEntries;
+    }
+   
+}
