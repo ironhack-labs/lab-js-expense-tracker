4cfee53751f306f8157f62b001eeb4708e575cba
Claudia Pando <claudiamfp@hotmail.com>
2024-10-07 16:58:10 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,67 @@
 // Entry
-class Entry {}
+class Entry {
+
+    constructor( date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+      }
+      getFormattedAmount() {
+        return this.amount + ' €';
+      }
+}
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor( date, amount, description){
+    super(date, amount, description);
+    this.type = 'income';
+}
+}
+
+
+
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor( date, amount, description, paid ){
+    super(date, amount, description);
+    this.type = 'expense';
+    this.paid = paid;
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`};
+}
+
+
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(newEntry){
+    this.entries.push(newEntry);
+    }
+
+
+    getCurrentBalance(){
+        let totalIncome = 0;
+        let totalExpense = 0;
+
+this.entries.forEach (function(entry) {
+    if(entry.type === 'income') {
+        totalIncome+= entry.amount;
+    }
+    else if (entry.type === 'expense') {
+        totalExpense+= entry.amount;
+    }
+});
+    return totalIncome - totalExpense;
+    }
+}
+   
+
+
