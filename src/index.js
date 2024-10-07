66bda90133f752a3818802c55e065208520cda9c
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-10-07 15:17:06 +0200

    balance lab Olga

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, category) {
+    super(date, amount, description);
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.type = "expense";
+    this.paid = category;
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(newEntry) {
+    this.entries.push(newEntry);
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) {
+      return 0;
+    } else {
+      return;
+    }
+  }
+}
