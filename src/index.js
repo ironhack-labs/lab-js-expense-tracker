354a20b3e820f861df1d176f3284a922a66476f0
luna <lunapadilla.8@gmail.com>
2024-10-07 18:07:06 +0200

    add solution Iteration 0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 // Entry
-class Entry {}
-
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
+const entry1 = new Entry("", 199, "hola");
+console.log(entry1.getFormattedAmount());
 // Income
 class Income {}
 
