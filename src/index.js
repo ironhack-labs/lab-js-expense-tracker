956db76ba916b6e877ab2cfe6df1c54184d34aa1
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-07 21:56:14 +0200

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount(){
+        return `${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry{
+    constructor(date,amount,description){
+        super(date,amount,description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor(date,amount,description, paid){
+        super(date,amount,description);
+        this.type = "expense";
+        this.paid = paid;
+    }
+
+    getFormattedAmount(){
+        return `-${this.amount} €`
+
+
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(Entry){
+        this.entries.push(Entry);
+    }
+
+    getCurrentBalance(){
+        if(this.entries.length === 0)
+            return 0;
+        let Balance = 0;
+        for(let i = 0; i< this.entries.length; i++){
+            if(this.entries[i].type === "expense"){
+                Balance -= this.entries[i].amount;
+            }else{
+                Balance += this.entries[i].amount;
+
+            }
+        }
+        return Balance;
+    }
+    getFormattedEntries(){
+        let formattedEntries = [];
+        this.entries.forEach((entry)=>{
+            formattedEntries.push(entry.date + " | "+entry.description + " | "+entry.getFormattedAmount());
+        })
+        return formattedEntries;
+    }
+}
+
