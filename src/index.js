817643ab2a9194652156a1c81fe91734c8038cf7
luna <lunapadilla.8@gmail.com>
2024-10-07 19:07:10 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -9,13 +9,48 @@ class Entry {
     return `${this.amount} €`;
   }
 }
-const entry1 = new Entry("", 199, "hola");
-console.log(entry1.getFormattedAmount());
+
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getCurrentBalance() {
+    let totalIncome = 0;
+    let totalExpense = 0;
+    if (this.entries.length === 0) {
+      return 0;
+    }
+    this.entries.forEach((entry) => {
+      if (entry.type === "income") {
+        totalIncome += entry.amount;
+      } else if (entry.type === "expense") {
+        totalExpense += entry.amount;
+      }
+    });
+    return totalIncome - totalExpense;
+  }
+}
