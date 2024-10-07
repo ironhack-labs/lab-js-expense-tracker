414feccebf9e86c0bd3c02f1c9a5fc2b8f2a423d
nathidaum <nathidaum@gmail.com>
2024-10-07 11:55:39 +0200

    solved iteration income & expense

--- src/index.js
+++ src/index.js
@@ -11,10 +11,34 @@ class Entry {
 }
 
 // Income
-class Income {}
+
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.date = date;
+        this.amount = amount;
+        this.description =  description;
+        this.type = "income";
+    };
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.date = date;
+        this.amount = amount;
+        this.description =  description;
+        this.type = "expense";
+        this.paid = paid;
+    };
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Budget
 class Budget {}
