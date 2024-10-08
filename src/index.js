8172cbadd6f222e2e352491f4c40e81febba7185
Ignacio Molina <molinanacho33@gmail.com>
2024-10-08 18:36:59 +0200

    Finish Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+
+    getFormattedAmount() {
+        return  `+${super.getFormattedAmount()}`;
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+
+    getFormattedAmount() {
+        return  `-${super.getFormattedAmount()}`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+    
+    addEntry(newEntry) {
+        this.entries.push(newEntry);
+    }
+
+    getCurrentBalance() {
+        let balance = 0;
+
+        this.entries.forEach(entry => {
+            if (entry.type === "income") {
+                balance +=  entry.amount;
+            } else if(entry.type === "expense") {
+                balance -= entry.amount;
+            }
+        });
+
+        return balance;
+    }
+
+    getFormattedEntries() {
+        const formattedEntries = [];
+        this.entries.forEach(entry => {
+            formattedEntries.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`);  
+        });
+
+        return formattedEntries;
+    }
+}
