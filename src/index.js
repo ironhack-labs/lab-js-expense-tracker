8d01ba18186aba3e32705b8a2a7b8da54f29317b
Mariana Frazão <mariana.franco.frazao@gmail.com>
2024-10-08 23:14:57 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
 // Entry
-class Entry {}
-
+class Entry {
+    constructor(date, amount, description){
+        this.date = date;
+        this.amount =  amount;
+        this.description = description;
+    }
+    getFormattedAmount(){
+        return `${this.amount} €`
+    }
+}
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description)
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid){
+        super(date, amount, description)
+        this.type = "expense";
+        this.paid = paid;
+    }
+    getFormattedAmount(){
+        return `-${this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries=[];
+    }
+    addEntry(entry){
+        this.entry = entry;
+        this.entries.push(entry)
+    }
+    getCurrentBalance(){
+        let resultExpense = 0;
+        let resultIncome = 0;
+        for (let i=0; i < this.entries.length; i++){
+            if (this.entries[i].type === "income"){
+                resultIncome += this.entries[i].amount;
+            } else if (this.entries[i].type === "expense"){
+                resultExpense += this.entries[i].amount;
+            }
+        }
+        return resultIncome - resultExpense;
+    }
+}
