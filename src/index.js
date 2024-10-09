3484830bf8ee3c4b660540d46d9858d09b193814
fiona-eh <fionahulse@mac.com>
2024-10-09 10:42:32 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount(){
+        return this.amount + " €"
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+
+    }
+    getFormattedAmount(){
+         return '-' + this.amount + " €"
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    };
+
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+
+    getCurrentBalance(){
+        if (!this.amount){
+            return 0;
+        }
+        let sumIncome = 0;
+        let sumExpense = 0;
+
+        for (let i=0; i<this.entries.length; i++){
+            if (entry.type === "income") {
+            sumIncome += this.entries[i].amount
+        }
+        else {
+            sumExpense += this.entries[i].amount
+        }
+
+        return sumIncome - sumExpense
+        
+    }
+}
+}
