9f395917c612837dc59678212928964beb01a87a
RoniPG <ronigmn@gmail.com>
2024-10-11 15:53:13 +0200

    Iteration 3 | Expense

--- src/index.js
+++ src/index.js
@@ -5,21 +5,30 @@ class Entry {
         this.amount = amount;
         this.description = description;
     }
-    getFormattedAmount () {
+    getFormattedAmount() {
         return `${this.amount} €`;
     }
 }
 
 // Income
-class Income extends Entry{
-    constructor(date,amount,description) {
-        super(date,amount,description)
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
         this.type = "income";
     }
 }
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget { }
