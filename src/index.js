e32ad26fa377dafe380c7a9c561dfb47c7b23bd8
RoniPG <ronigmn@gmail.com>
2024-10-11 15:45:21 +0200

    Iteration 2 | Income

--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ class Entry {
 }
 
 // Income
-class Income {}
+class Income extends Entry{
+    constructor(date,amount,description) {
+        super(date,amount,description)
+        this.type = "income";
+    }
+}
 
 // Expense
 class Expense {}
