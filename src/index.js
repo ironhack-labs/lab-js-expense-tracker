311d285954d92e252a4b9a216531369af5b4da57
Nacho Monzón <ignaciomonzona@gmail.com>
2024-10-13 19:40:29 +0200

    Primera iteración terminada

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date; 
+        this.amount = amount;
+        this.description = description; 
+    }
+
+getFormattedAmount() {
+
+   return `${this.amount} €`
+}
+
+
+}
 
 // Income
 class Income {}
