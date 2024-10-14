155c868aa4e4cf70459cc77f0a696675d38a0760
hurtaditoo <al415507@uji.es>
2024-10-14 13:14:41 +0200

    Iteration 0: First test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+}
 
 // Income
 class Income {}
