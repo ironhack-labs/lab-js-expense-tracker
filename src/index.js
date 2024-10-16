34e065828073d612b430f536dfdc2bf89b84cf2c
Carla Lopez <iamcarla.28@gmail.com>
2024-10-16 18:17:58 +0200

    Finished lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date, amount, description) {
+        this.date = date; 
+        this.amount = amount; 
+        this.description = description; 
+    }
+
+    getFormattedAmount(){
+        return (`${this.amount} €`);
+    }
+
+}   
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor (date, amount, description) {
+        super(date); 
+        this.amount = amount; 
+        this.description = description;
+        this.type = 'income'; 
+        this.amountSign = amount; 
+    }  
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid) {
+        super(date); 
+        this.amount = amount; 
+        this.description = description;
+        this.paid = paid;  
+        this.type = 'expense';
+        this.amountSign = - amount; 
+
+    }
+    getFormattedAmount(){
+        return (`-${this.amount} €`);
+    }
+}
+
 
 // Budget
-class Budget {}
+class Budget {
+    constructor (){
+        this.entries=[];
+    }
+
+    addEntry(entry){
+        this.entries.push(entry); 
+    }
+
+    getCurrentBalance(){
+        if (this.entries.length === 0){
+            return 0;
+        }
+     const balance = this.entries.reduce((acc, entry) => {
+        return acc + entry.amountSign;
+     },0); 
+     return balance;
+    }
+    
+    
+    getFormattedEntries(){
+        const formatEntries=[];
+        this.entries.forEach((entry ) => {
+            formatEntries.push(`${entry.date} | ${entry.description} | ${entry.amountSign} €`)
+        }); 
+        return formatEntries; 
+    }
+}
