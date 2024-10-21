297ba485a76ccf0b26717fdfd6eea5ad01345cd7
Daniela Di Donato Vassallo <didonatodani@gmail.com>
2024-10-21 12:54:37 +0200

    first iteration done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+console.clear()
+
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date= date,
+        this.amount= amount,
+        this.description = description
+    }
+    getFormattedAmount(){
+        return `${this.amount} €`
+    }
+}
 
 // Income
 class Income {}
