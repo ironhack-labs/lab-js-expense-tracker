20c59c9be41c40e5da0531e6c64f2422811dc802
Mr Footwork <pandau.ting@gmail.com>
2024-10-23 02:08:09 +0200

    iteration 1 done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income { }
 
 // Expense
-class Expense {}
+class Expense { }
 
 // Budget
-class Budget {}
+class Budget { }
