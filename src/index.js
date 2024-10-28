b9138a77522e397d57f73a6b11f367bf328b8d45
machmaegal <aka.pleat371@passinbox.com>
2024-10-28 15:33:00 +0100

    done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,76 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    } 
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor (date, amount, description) {
+        super(date, amount, description)
+        this.type = "income";
+    }  
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid) {
+        super(date, amount, description)
+        this.paid = paid;
+        this.type= "expense";
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    } 
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(newEntry) {
+        this.entries.push(newEntry)
+    }
+
+    getCurrentBalance() {
+        if (this.entries.length === 0) return 0;
+
+        let expense = 0;
+        let income = 0;
+
+        this.entries.forEach( entry => {          
+            
+            if (entry.type === "expense") {
+                expense += entry.amount;
+            } else {
+                income += entry.amount;
+            }
+        } )
+        
+    return income - expense;
+    }
+
+    getFormattedEntries () {
+
+        const variabel = this.entries.reduce((entries, entry) => {
+
+            entries.push( `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}` )
+
+            return entries;
+        }, [] )       
+
+        return variabel;
+       
+    }
+}
+
