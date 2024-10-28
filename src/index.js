6186b5c904c1cd8493c38fd0fc38e16f6877ded7
Robert Ortiz <84008858+last-orbit@users.noreply.github.com>
2024-10-28 15:28:10 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,66 @@
 // Entry
-class Entry {}
-
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
+// const amount = new Entry(20,100,'bought toys')
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description, type = 'income') {
+    super(date, amount, description);
+    this.type = type;
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid, type = 'expense') {
+    super(date, amount, description);
+    this.type = type;
+    this.paid = paid;
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getCurrentBalance() {
+    if (this.entries.length === 0) return 0;
+    let totalIncome = 0;
+    let totalExpense = 0;
+    for (let i = 0; i < this.entries.length; i++) {
+      if (this.entries[i].type == 'income') {
+        totalIncome += this.entries[i].amount;
+      } else if (this.entries[i].type == 'expense') {
+        totalExpense += this.entries[i].amount;
+      }
+    }
+    return totalIncome - totalExpense;
+  }
+  //Iteration 6
+  getFormattedEntries() {
+    let entries = [];
+    this.entries.forEach((entry) => {
+      entries.push(
+        `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`
+      );
+    });
+    // return formattedEntries;
+    // console.log(entries);
+    return entries;
+  }
+}
