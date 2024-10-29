e31b1d7b1644ac9262fca27e68fcaaab98bbea2e
vicmarchesi <victor.marchesi91@gmail.com>
2024-10-29 20:32:15 +0000

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = paid;
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+    getCurrentBalance(){
+        return this.entries.reduce((balance, entry) => {
+          return balance + (entry.type === 'income' ? entry.amount : -entry.amount);
+        }, 0);
+      }
+    }
