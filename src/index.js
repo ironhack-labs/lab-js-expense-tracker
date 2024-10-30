5399cbe51f7f537c6472dc6ad81da1d142f4a2bc
Gonçalo Furtado <gfurtado.sw@gmail.com>
2024-10-30 10:04:32 +0000

    almost solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+
+}
 
 // Income
-class Income {}
+class Income extends Entry{
+    constructor(date, amount, description) {
+    super (date, amount, description) 
+    this.amount = amount;
+    this.description = description;
+    this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid) {
+        super (date)
+        this.amount = amount;
+        this.description = description;
+        this.paid = paid;
+        this.type = "expense";
+    }
+    getFormattedAmount() {
+    return ` -${this.amount} €`;
+    }
+}
+const internet = new Expense ("internet", 2024, 1,);
+    console.log(cost.getFormattedAmount());
+
 
 // Budget
 class Budget {}
