05696e50beb4db414db3aad096b021bd4ddf8be2
brunellanmcarvalho1 <brunella.nmcarvalho1@gmail.com>
2024-10-31 07:17:40 +0100

    Solved Lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(newEntry) {
+    this.newEntry = newEntry;
+    this.entries.push(newEntry);
+  }
+
+  getCurrentBalance() {
+    if ((this.entries = [])) {
+      return 0;
+    }
+    let balance = 0;
+    this.entries.forEach((entry) => {
+      if (entries.type === "income") {
+        balance += entries.value;
+      } else {
+        balance -= this.entries.value;
+      }
+    });
+    return balance;
+  }
+}
