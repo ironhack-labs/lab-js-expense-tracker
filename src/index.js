9877a49b9dfa873545fc85dbe668c05dc33bdcbe
Mariah <alinemariah@hotmail.com>
2024-11-25 20:28:33 +0100

    javascript expense tracker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,75 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date, amount, description){
+       this.date = date;
+       this.amount = amount;
+       this.description = description;
+
+    }
+
+    getFormattedAmount(){
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor (date, amount, description){
+        super(date, amount, description);
+        this.type = "income";
+    } 
+
+    
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid){
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+    
+    getFormattedAmount(){
+        return `-${this.amount} €`;
+    }
+
+}
+
 
 // Budget
-class Budget {}
+class Budget {
+    constructor (){
+        this.entries = [];
+    }
+
+    addEntry(entry){
+        this.entries.push(entry)
+    }
+
+    getCurrentBalance(){
+        let totalIncome = 0;
+        let totalExpense = 0;
+
+        this.entries.forEach((entry) => {
+            if (entry.type === "income"){
+                totalIncome +=entry.amount;
+            } else if (entry.type === "expense"){
+                totalExpense +=entry.amount;
+            }
+        });
+        return totalIncome - totalExpense;
+    }   
+
+    getFormattedEntries(){
+        const formattedEntries = []
+        this.entries.forEach(entry => {
+            formattedEntries.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`)
+        })
+        return formattedEntries;
+    }
+
+}
+
+
+
