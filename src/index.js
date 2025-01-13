fa59e9ed049f1901c6d473495b71c96c484b0b9a
Nikol_Niven <ntopalusic@gmail.com>
2025-01-13 15:06:01 +0100

    till third iteration

--- src/index.js
+++ src/index.js
@@ -6,15 +6,31 @@ class Entry {
     this.description = description;
   }
   getFormattedAmount() {
-    const amountEuro = `${this.amount}€`;
+    const amountEuro = `${this.amount} €`;
+    return amountEuro;
   }
 }
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description); // do I have to always repeat the keywords of the main class constructor? or could I have also wrote just type here in this case?
+    this.type = "income";
+  }
+}
 
-// Expense
-class Expense {}
+Expense;
+class Expense extends Entry {
+  constructor(date, amount, description, income) {
+    super(date, amount, description, income);
+    this.type = paid;
+  }
+
+  getFormattedAmount() {
+    const amountEuroMinus = `-${this.amount} €`;
+    return amountEuroMinus;
+  }
+}
 
 // Budget
 class Budget {}
