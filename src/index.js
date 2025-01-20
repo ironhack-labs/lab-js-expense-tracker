acb5f52a518e5a1a5cc4bda0dbaa6ea0804c04df
Samuel Onoja <samuelonoja0@gmail.com>
2025-01-20 08:53:36 -0800

    lab done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,93 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount(){
+        return  this.amount + " €";
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super()
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+        this.type = "income"
+    }
+    
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor(date, amount, description,paid){
+        super()
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+        this.paid = paid;
+        this.type = "expense"
+    }
+
+    getFormattedAmount(){
+        return  `-${this.amount} €`
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(Income){
+        this.entries.push(Income)
+    }
+
+    getCurrentBalance(){
+        if(this.entries.length === 0){
+            return 0;
+        }
+        else{
+            /*
+            getCurrentBalance(){
+       if(this.entries.length === 0 || !this.entries){
+             return 0
+       }
+        const tempIncome = []
+        const tempExpense = []
+        for (const val of this.entries){
+            if (val.type === "income"){
+                tempIncome.push(val.amount)
+            }
+            if (val.type === "expense") {
+                tempExpense.push(val.amount)
+            }
+        }
+
+       
+       let sumIncom = 0
+       let sumExpense = 0
+       for (const val of tempIncome){
+            sumIncom += val
+       }
+       for (const val of tempExpense){
+              sumExpense += val
+       }
+       const balance = sumIncom - sumExpense
+       return balance
+    }
+            */
+        }
+    }
+
+    getFormattedEntries(){
+        return this.entries.map(entry => {
+            return `${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`;
+        })
+    }
+}
