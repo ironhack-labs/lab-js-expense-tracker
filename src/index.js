6f06b13f537e440edc6e44196bcae2b637a24ecf
Johana Castellano <johanacg16@gmail.com>
2025-01-20 11:56:01 +0000

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,73 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+      super(date, amount, description);
+      this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+      super(date, amount, description);
+      this.type = "expense";
+      this.paid = paid;
+    }
+
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(entry) {
+        return this.entries.push(entry);
+    }
+
+    getCurrentBalance() {
+        let balance = 0;
+        if (!this.entries.length) return balance;
+
+        this.entries.forEach(entry => {
+            if (entry.type === "income") {
+                balance += entry.amount;
+            } else if (entry.type === "expense") {
+                balance -= entry.amount;
+            }
+        });
+        
+        return balance;
+    }
+
+    getFormattedEntries() {
+        let formattedEntries = [];
+
+        this.entries.forEach(entry => {
+            if (entry.type === "income") {
+                formattedEntries.push(`${entry.date} | ${entry.description} | ${entry.amount} €`);
+            } else if (entry.type === "expense") {
+                formattedEntries.push(`${entry.date} | ${entry.description} | -${entry.amount} €`);
+            }
+        });
+
+        return formattedEntries;
+    }
+}
