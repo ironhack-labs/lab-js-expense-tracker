af192846b9badcdcba625e8093e82e95bd20e2ec
Kamran Ali <kamranali130893@gmail.com>
2025-01-20 16:02:07 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,81 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return this.amount + " €";
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super();
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.type = "income";
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super();
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    this.paid = paid;
+    paid = 0;
+    this.type = "expense";
+  }
+  getFormattedAmount() {
+    return -this.amount + " €";
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(entry) {
+    this.entries.push(entry);
+  }
+  getCurrentBalance() {
+    let income = 0;
+    let expense = 0;
+    if (this.entries.length === 0) {
+      return 0;
+    } else {
+      this.entries.forEach(function (entry) {
+        console.log(entry);
+        if (entry.type === "income") {
+          income += entry.amount;
+        } else if (entry.type === "expense") {
+          expense += entry.amount;
+        }
+      });
+      return income - expense;
+    }
+  }
+  getFormattedEntries() {
+    let formattedArr = [];
+    let income = [];
+    let expense = [];
+    this.entries.forEach(function (entry) {
+      //   formattedArr.push(entry);
+      if (entry.type === "income") {
+        entry.amount = entry.amount + " €";
+        income.push(entry);
+      } else if (entry.type === "expense") {
+        entry.amount = entry.amount + " €";
+        expense.push(entry);
+      }
+    });
+    return formattedArr;
+  }
+}
