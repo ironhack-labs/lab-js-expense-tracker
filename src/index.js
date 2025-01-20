ee3b1b86beb6c4ea14b7af7177790199603009de
Dvdrepetto <davidmrepetto@gmail.com>
2025-01-20 19:18:11 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return this.amount + ' €';
+    }
+}
+const entry1 = new Entry()
 
 // Income
-class Income {}
-
+class Income extends Entry {
+    constructor(date, amount, description,) {
+        super(date, amount, description);
+        this.type = 'income';
+    }
+}
 // Expense
-class Expense {}
+class Expense extends Income {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description, paid);
+        this.type = 'expense';
+        this.paid = paid;
+    }
+    getFormattedAmount() {
+        return -this.amount + ' €';
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+    addEntry(Income) {
+        this.entries.push(Income);
+    }
+    getCurrentBalance() {
+        if (this.entries.length === 0) {
+            return 0;
+        }
+        let totalIncome = 0;
+        let totalExpenses = 0;
+        this.entries.forEach(function (entry) {
+            if (entry.type === "income") {
+                totalIncome += entry.amount;
+            } else if (entry.type === "expense") {
+                totalExpenses += entry.amount;
+            }
+        });
+        return totalIncome - totalExpenses;
+    }
+    getFormattedEntries() {
+    }
+}
+
