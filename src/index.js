12205fec0a62af5dd9256aaeeccb83b6caa4f43e
Goncalo Neves <goncaloneves92@gmail.com>
2025-02-07 09:00:40 +0000

    until iteration 4 addEntry() method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 // Entry
-class Entry {}
+class Entry  {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income"; 
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense";
+    }
+    getFormattedAmount(){
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(newEntry){
+        this.entries.push(newEntry);
+    }
+    getCurrentBalance(){
+        if (this.entries.length === 0){
+            return 0;
+    }
+
+    let totalIncome = 0;
+    let totalExpenses = 0;
+
+}
+}
