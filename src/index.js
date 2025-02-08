f68a3374972cb6a62c087815bb56d25837f74000
znguye <dnguyen.jmblc@gmail.com>
2025-02-08 17:29:34 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,65 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }   
+    getFormattedAmount(){
+        return `${this.amount} €`
+    };
+}
+
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+        super(date, amount, description);
+        this.paid = paid;
+        this.type = "expense"
+    }
+    getFormattedAmount(){
+        return `-${this.amount} €`
+    };
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+
+    getCurrentBalance(){
+        return this.entries.reduce((total, entry) =>
+        {
+            if (entry.type === "income"){
+                return total + entry.amount;
+            } else if (entry.type === "expense"){
+                return total - entry.amount;
+            } return total;
+        } ,0)
+    }
+    // getFormattedEntries()
+    getFormattedEntries(){
+        return this.entries.map(entry => {
+            if (entry.type === "income"){
+                return `${entry.date} | ${entry.description} | ${entry.amount} €`;
+            } else if (entry.type === "expense"){
+                return `${entry.date} | ${entry.description} | -${entry.amount} €`;
+            } 
+           return entry; 
+        })
+    } 
+}
