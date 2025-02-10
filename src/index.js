90e151d5f6a986107b202db7430d3f91050fa308
Juan Marin <jmo129@gmail.com>
2025-02-10 15:59:54 +0100

    Solved lab

--- src/index.js
+++ src/index.js
@@ -44,7 +44,6 @@ class Budget {
         this.entries = [];
     }
 
-
     // Métodos
 
     addEntry(nuevaEntrada) {
@@ -60,19 +59,48 @@ class Budget {
         let totalExpense = 0;
         let balance = 0;
 
-        for (entradas in this.entries) {
-            if(this.entries[entradas] === "income") {
-                totalIncome += entries[entradas][0];
+        for (let i=0; i<this.entries.length; i++) {
+            if(this.entries[i].type === "income") {
+                totalIncome += this.entries[i].amount;
             }
-            else if(this.entries[entradas] === "expense") {
-                totalExpense += entries[entradas][0];
+            else if(this.entries[i].type === "expense") {
+                totalExpense += this.entries[i].amount;
             }
         }
-        console.log(this.entries);
 
-        console.log(totalIncome + " Total Ingresos");
+        //console.log(this.entries);
+
+        //console.log(totalIncome + " Total Ingresos");
         balance = totalIncome - totalExpense;
         return balance;
+    }
+
+    getFormattedEntries() {
+        let listaEntradas = [];
+
+        for(let i=0;i<this.entries.length;i++) {
+            let entradaNuevoFormato = "";
+
+
+            // Añadimos al nuevo string el nuevo formato y hacemos push para introducirlo en el array de listaEntradas
+
+            if(this.entries[i].type === "income") {
+                 entradaNuevoFormato = `${this.entries[i].date} | ${this.entries[i].description} | ${this.entries[i].amount} €`;
+                 listaEntradas.push(entradaNuevoFormato);
+
+                
+            }
+
+            // Lo mismo con "expense"
+            if(this.entries[i].type === "expense") {
+                entradaNuevoFormato = `${this.entries[i].date} | ${this.entries[i].description} | -${this.entries[i].amount} €`;
+                listaEntradas.push(entradaNuevoFormato);
+           }
+        }
+
+        // Devolvemos el nuevo array con el nuevo formato
+        console.log(listaEntradas);
+        return listaEntradas;
     }
 
 }
