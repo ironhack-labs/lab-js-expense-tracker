321394eae5aeeaf933aebe74ebc06c6b9c4809b6
CannyRo <whatcannydev@gmail.com>
2025-02-24 14:46:15 +0100

    Lab solved by Ronan CANNY

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount() {
+        return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = paid;
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+    addEntry(oneEntry) {
+        this.entries.push(oneEntry);
+    }
+    getCurrentBalance() {
+        let balance = 0;
+        this.entries.forEach( entry => {
+            entry.type === 'income' ? balance += entry.amount : balance -= entry.amount;
+        })
+        return balance;
+    }
+    getFormattedEntries() {
+        const formattedDatas = [];
+        this.entries.forEach( entry => {
+            entry.type === "income" ? 
+            formattedDatas.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`) : 
+            formattedDatas.push(`${entry.date} | ${entry.description} | ${entry.getFormattedAmount()}`);
+        })
+        return formattedDatas;
+    }
+}
