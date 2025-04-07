6d8cee99f77060241f9b409376f0d37b832ac530
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-04-07 16:34:36 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -1,11 +1,61 @@
 // Entry
-class Entry {}
+class Entry {
+  constructor(date, amount, description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+  getFormattedAmount() {
+    return `${this.amount} €`;
+  }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+  constructor(date, amount, description) {
+    super(date, amount, description);
+    this.type = 'income';
+  }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+  constructor(date, amount, description, paid) {
+    super(date, amount, description);
+    this.paid = paid;
+    this.type = 'expense';
+  }
+  getFormattedAmount() {
+    return `-${this.amount} €`;
+  }
+}
 
 // Budget
-class Budget {}
+class Budget {
+  constructor() {
+    this.entries = [];
+  }
+  addEntry(newEntry) {
+    this.entries.push(newEntry);
+    console.log('entriesArr: ', this.entries);
+  }
+  getCurrentBalance() {
+    if (this.entries.length > 0) {
+      let totalIncome = 0;
+      let totalExpense = 0;
+
+      this.entries.forEach(function (element) {
+        console.log('element: ', element);
+        if (element.type === 'income') {
+          totalIncome += element.amount;
+        } else {
+          totalExpense += element.amount;
+        }
+      });
+
+      return totalIncome - totalExpense;
+    } else {
+      return 0;
+    }
+  }
+}
