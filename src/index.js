057716a1cf2955639f4eb43474d0b6de26dec611
Ramon Salva Sanchez <ramonsalvasanchez@gmail.com>
2025-04-10 20:05:49 +0200

    feed: completed iteration 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // Entry
 class Entry {
 
-	constructor(date, amount, description) {}
+	constructor(date, amount, description) {
+		this.date = date;
+		this.amount = amount;
+		this.description = description;
+	}
+
+	getFormattedAmount() {
+		return `${this.amount} €`;
+	}
 
 }
 
