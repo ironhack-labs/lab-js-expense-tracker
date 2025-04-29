302e1253b269cad44fb0c339549f85feaa24daa6
María <xmaryjs99@gmail.com>
2025-04-29 20:27:45 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor (date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount () {
+        return `-${this.amount} €`
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry{ 
+    constructor (date, amount, description ) {
+        super (date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor (date, amount, description, paid) {
+        super (date, amount, description, paid);
+        this.paid = paid;
+        this.type = "expense";
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+
+    constructor () {
+        this.entries = [];
+    }
+
+    addEntry(entry){
+        this.entries.push(entry);
+    }
+
+    getCurrentBalance() {
+    if (this.entries.length === 0) {
+        return 0;
+        }
+    let balance = this.type.income - this.type.expense;
+    return balance;
+}
+};
