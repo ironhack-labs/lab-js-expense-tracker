43a49ba89a634804cc4210ce12fdc3e22d9e41df
Alba Perez <albaperezbaldo@gmail.com>
2025-05-11 18:31:21 +0200

    lab done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
 // Entry
-class Entry {}
+class Entry {
+
+      constructor(date, amount, description) {
+
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+
+      }
+      getFormattedAmount() {
+        return `${this.amount} €`;
+      }
+
+}
 
 // Income
-class Income {}
+class Income extends Entry{
+    constructor(date, amount, description){
+        super(date, amount, description)
+        this.type = "income";
+    }
+
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry{
+    constructor(date, amount, description, paid) {
+        super(date, amount, description);
+        this.type = "expense";
+        this.paid = paid;
+    }
+    getFormattedAmount() {
+        return `-${this.amount} €`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+    getCurrentBalance() {
+       if(this.entries.length === 0){
+        return 0;
+       }
+     let balance = 0;
+
+     for(let entry of this.entries){
+        if(entry.type === "income") {
+            balance += entry.amount;
+        }
+        else if(entry.type === "expense") {
+            balance -= entry.amount;
+        }
+     }
+
+     return balance;
+    }
+}
