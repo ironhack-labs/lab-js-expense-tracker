8b32ef56c2d6071b8b8b8557ec8739b330ce30ba
Rishi G. <mail.rishig@gmail.com>
2025-05-12 15:20:55 +0200

    labe done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
+class Entry {
 // Entry
-class Entry {}
+    constructor(date, amount, description, ){
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+    }
+    getFormattedAmount() {
+       return `${this.amount} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date,amount,description,paid){
+        super(date,amount,description);
+        this.type = "expense";
+        this.paid = paid ;
+    }
+ getFormattedAmount() {
+    return `-${this.amount} €`;
+ }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor(){
+        this.entries = [];
+    }
+addEntry(entry){
+    this.entries.push(entry)
+}
+    getCurrentBalance(){
+        let total = (this.type.Income - this.type.Expense);
+        return total;
+        
+        }
+    }
+
+
